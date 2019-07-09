92783d1ce83c1230dd6801c5653f4099d3f968cf
Nemanja <nemadjo94@gmail.com>
2019-07-09 09:49:05 +0200

    Add files via upload

---
+++ Act10_1/Author.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Act10_1
+{
+    class Author
+    {
+        private SqlConnection pubConnection;
+        private string connString;
+
+
+        //Constructor
+        public Author()
+        {
+            connString = "Data Source=localhost;Initial Catalog=pubs;Integrated Security=True";
+            pubConnection = new SqlConnection();
+            pubConnection.ConnectionString = connString;
+        }
+
+        public int CountAuthors()
+        {
+            try
+            {
+                SqlCommand pubCommand = new SqlCommand();
+                pubCommand.Connection = pubConnection;
+                pubCommand.CommandText = "Select Count(au id) from authors";
+                pubConnection.Open();
+                return (int)pubCommand.ExecuteScalar();
+            }
+            catch (SqlException exc)
+            {
+                throw exc;
+            }
+            finally
+            {
+                if(pubConnection != null)
+                {
+                    pubConnection.Close();
+                }
+            }
+        }
+
+        public List<string> GetAuthorList()
+        {
+            SqlCommand authorsCommand = new SqlCommand();
+            SqlDataReader authorDataReader;
+            List<string> nameList = new List<string>();
+
+            try
+            {
+                authorsCommand.Connection = pubConnection;
+                authorsCommand.CommandText = "Select au lname from authors";
+                pubConnection.Open();
+                authorDataReader = authorsCommand.ExecuteReader();
+
+                while(authorDataReader.Read() == true)
+                {
+                    nameList.Add(authorDataReader.GetString(0));
+                }
+                return nameList;
+            }
+            catch(SqlException exc)
+            {
+                throw exc;
+            }
+            finally
+            {
+                if (pubConnection != null)
+                {
+                    pubConnection.Close();
+                }
+            }
+        }
+
+        public List<string> GetAuthorList(int royalty)
+        {
+            SqlCommand authorsCommand = new SqlCommand();
+            SqlDataReader authorDataReader;
+            List<string> nameList = new List<string>();
+            SqlParameter inputParameter = new SqlParameter();
+
+            try
+            {
+                authorsCommand.Connection = pubConnection;
+                authorsCommand.CommandText = "SELECT au lname FROM authors";
+                pubConnection.Open();
+                authorDataReader = authorsCommand.ExecuteReader();
+
+                while(authorDataReader.Read() == true)
+                {
+                    nameList.Add(authorDataReader.GetString(0));
+                }
+                return nameList;
+            }
+            catch (SqlException exc)
+            {
+                throw exc;
+            }
+            finally
+            {
+                if(pubConnection != null)
+                {
+                    pubConnection.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Act10_1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act10_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Author author = new Author();
+            //    Console.WriteLine(author.CountAuthors());
+            //    Console.ReadLine();
+            //}
+            //catch(Exception exc)
+            //{
+            //    Console.WriteLine(exc.Message);
+            //    Console.ReadLine();
+            //}
+
+
+            try
+            {
+                Author author = new Author();
+                foreach (string name in author.GetAuthorList())
+                {
+                    Console.WriteLine(name);
+                }
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+    }
+}
